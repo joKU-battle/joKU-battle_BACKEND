9f48013b98c56287a98dbf6e3c7841b85411f8a7
ajroot5685 <anjeong8577@naver.com>
2025-01-01 22:35:33 +0900

    initial commit

---
+++ src/test/java/konkuk/jokubattle/JokubattleApplicationTests.java
@@ -0,0 +1,13 @@
+package konkuk.jokubattle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokubattleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
