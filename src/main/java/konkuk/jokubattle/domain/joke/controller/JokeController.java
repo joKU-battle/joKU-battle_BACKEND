6dd1e2a083267e4bafb015e99c23879b7b3709bf
ajroot5685 <anjeong8577@naver.com>
2025-01-04 03:41:50 +0900

    fix: 빌드 오류 수정

--- src/main/java/konkuk/jokubattle/domain/joke/controller/JokeController.java
+++ src/main/java/konkuk/jokubattle/domain/joke/controller/JokeController.java
@@ -2,6 +2,7 @@ package konkuk.jokubattle.domain.joke.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
 import konkuk.jokubattle.domain.joke.dto.request.JokeRequestDto;
 import konkuk.jokubattle.domain.joke.dto.response.JokeResponseDto;
 import konkuk.jokubattle.domain.joke.service.JokeService;
@@ -10,9 +11,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "우스운 말", description = "우스운 말 API")
 @RequiredArgsConstructor
@@ -36,8 +39,8 @@ public class JokeController {
         return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
     }
 
-    @Operation(summary = "월드컵 8강 진행", description = "우스운 말 투표를 시작합니다.")
-    @
+//    @Operation(summary = "월드컵 8강 진행", description = "우스운 말 투표를 시작합니다.")
+//    @
 
 
 }
